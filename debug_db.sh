5f3d3a2fac5d24690f195ddd646c9af1c32185ad
Drew Ogle <drew@aperturedata.io>
2022-09-22 14:22:11 -0400

    fix grep outputting

--- debug_db.sh
+++ debug_db.sh
@@ -5,5 +5,5 @@ echo "Running ${ADB} -cfg ${CFG}"
 # logs to standarde error
 # smushed stderr and stdout ( also seems to help with finishing faster? )
 # removes debug we don't care about.
-GLOG_logtostderr=1 "${ADB}" -cfg "${CFG}"  2>&1 | grep -v process_queue | grep -v PERIODIC
+GLOG_logtostderr=1 "${ADB}" -cfg "${CFG}"  2>&1 | grep --line-buffered -v process_queue | grep --line-buffered -v PERIODIC
 #| grep -v SSL_read
